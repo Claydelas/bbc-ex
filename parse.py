af47b85057fd712620f9e9d821bf55203e6c439c
Martin Valchev <mv1u18@soton.ac.uk>
2021-11-14 17:31:24 +0200

    refactor: script now importable as a module

--- parse.py
+++ parse.py
@@ -1,30 +1,45 @@
 import json
+import sys
 import pandas as pd
 
-data = pd.read_csv('./bg-sentence-pair-scores.csv')
 
-# average rating by evaluator id
-eval_avg = data.groupby('evaluator id')['q1 score'].mean()
+def export_results(data: dict, path: str):
+    with open(path, 'w') as file:
+        json.dump(data, file, indent=2)
 
-with open('1.json', 'w') as file:
-    json.dump(eval_avg.to_dict(), file, indent=2)
 
-# average rating for each sentence
-sent_avg = data.groupby('sentence pair id', sort=False)['q1 score'].mean()
+def analyse(path: str, export: bool = False):
+    data = pd.read_csv(path)
 
-# highest-scoring sentences
-max_val = sent_avg.max()
-max_series = sent_avg[sent_avg == max_val]
+    # Task 1 - average rating by evaluator id
+    eval_avg = data.groupby('evaluator id')['q1 score'].mean()
 
-# lowest-scoring sentences
-min_val = sent_avg.min()
-min_series = sent_avg[sent_avg == min_val]
+    # Task 2 - average rating for each sentence
+    sent_avg = data.groupby('sentence pair id', sort=False)['q1 score'].mean()
 
-# combined output
-result = {"avg": sent_avg.to_dict(),
-          "min": {"score": min_val, "sentences": min_series.index.to_list()},
-          "max": {"score": max_val, "sentences": max_series.index.to_list()}
-          }
+    # highest-scoring sentences
+    max_val = sent_avg.max()
+    max_series = sent_avg[sent_avg == max_val]
 
-with open('2.json', 'w') as file:
-    json.dump(result, file, indent=2)
+    # lowest-scoring sentences
+    min_val = sent_avg.min()
+    min_series = sent_avg[sent_avg == min_val]
+
+    if export:
+        export_results(eval_avg.to_dict(), '1.json')
+
+        # combined output
+        result = {"avg": sent_avg.to_dict(),
+                  "min": {"score": min_val, "sentences": min_series.index.to_list()},
+                  "max": {"score": max_val, "sentences": max_series.index.to_list()}
+                  }
+        export_results(result, '2.json')
+
+    return eval_avg, sent_avg, max_series, min_series
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        analyse(sys.argv[1], export=True)
+    else:
+        analyse('./bg-sentence-pair-scores.csv', export=True)
